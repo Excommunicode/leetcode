afe8e7f02082eb836ae1b4d724b8cee2983da3ce
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-02-27 02:49:08 +0600

    Time: 1 ms (99.87%), Space: 45.9 MB (70.39%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+
+    public int singleNumber(int[] nums) {
+        int unique = 0;
+        for (int num : nums) {
+            unique ^= num;
+        }
+        return unique;
+    }
+}
