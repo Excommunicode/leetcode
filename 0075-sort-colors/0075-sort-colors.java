a013eebf794d28f402d0d66c780994be5c39ab57
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-04-20 05:00:26 +0600

    Time: 2 ms (13.71%), Space: 41.7 MB (65.05%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+
+                break;
+            }
+        }
+    }
+}
