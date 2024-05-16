2636c53e3c4064dddc9e23544f50368b886b3899
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-05-16 23:19:12 +0500

    Time: 0 ms (100.00%), Space: 42.7 MB (72.88%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,22 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-       int x = 0;
-        int size = nums.length;
+      int size = nums.length;
         for (int i = 0; i < size; i++) {
             if (nums[i] == target) {
                 return i;
             } else {
                 if (nums[i] > target) {
-                    x = i;
-                    break;
-                }
-                if (nums[size -1] < target) {
-                    x = size;
-                    break;
+                    return i;
                 }
             }
         }
-        return x;
+        return nums[size - 1] < target ? size : -1;
         
     }
 }
