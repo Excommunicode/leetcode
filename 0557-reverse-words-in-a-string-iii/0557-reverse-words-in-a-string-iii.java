20d40589ff4a54756fe17b5325a1b094acc66531
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-05-20 23:23:03 +0500

    Time: 14 ms (31.28%), Space: 45.4 MB (31.71%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,12 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
         String[] words = s.split(" ");
-        StringBuilder string = new StringBuilder();
+        String string = "";
         for (String word : words) {
             StringBuilder reversedWord = new StringBuilder(word + " ");
             StringBuilder reverse = reversedWord.reverse();
-            string.append(reverse);
+            string += reverse;
         }
-        return string.toString().trim();
+        return string.trim();
     }
 }
