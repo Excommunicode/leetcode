f417837610acb2372587a6bd6eb6eae2e97519d9
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-05-21 02:54:36 +0500

    Time: 25 ms (5.03%), Space: 44.7 MB (96.05%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Set<Integer> numbers = new HashSet<>();
+        List<Integer> numbers = new ArrayList<>();
         for (int num : nums) {
             numbers.add(num);
         }
         List<Integer> collect = numbers.stream()
                 .sorted()
                 .toList();
-
         int x = 0;
         int i = collect.getFirst();
-        
         if (i == 1) {
             return 0;
         }
 
         while (i < collect.size()) {
-
             if (i != collect.get(x)) {
                 return i;
             }
