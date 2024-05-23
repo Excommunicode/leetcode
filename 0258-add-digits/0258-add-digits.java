1659902b9d63dc5a9b8eeb06cadfe520b5459855
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-05-23 17:05:14 +0500

    Time: 2 ms (47.52%), Space: 41.2 MB (7.23%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,11 +1,10 @@
 class Solution {
     public int addDigits(int num) {
         String s = String.valueOf(num);
-        String[] strings = s.split("");
         int x = 0;
         int y = 0;
-        while (x < strings.length) {
-            y += Integer.parseInt(strings[x]);
+        while (x < s.length()) {
+            y += Integer.parseInt(String.valueOf(s.charAt(x)));
             x++;
         }
         return y > 9 ? addDigits(y) : y;
