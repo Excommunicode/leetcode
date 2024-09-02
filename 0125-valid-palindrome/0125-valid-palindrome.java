65398849cddb88e0a50f42ac353c513a1aa54df2
Excommunicado <142714814+Farukh18@users.noreply.github.com>
2024-09-02 16:38:17 +0500

    Time: 14 ms (34.55%), Space: 44.8 MB (31.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s1 = s.toLowerCase().replace(" ", "").replaceAll("[^a-zA-Z0-9]", "");
+        String s1 = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         StringBuilder sss = new StringBuilder(s1);
         String string = sss.reverse().toString();
         return string.equals(s1);
