7ef5fb928ff49b6f349cc6f8b4532ab3a7fb90bb
Excommunicado <142714814+Excommunicode@users.noreply.github.com>
2024-10-02 11:48:47 +0500

    Time: 4 ms, Memory: 56 MB - LeetHub

--- maximum-average-subarray-i/maximum-average-subarray-i.java
+++ maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,9 +1,10 @@
+
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int leftIndex = 0;
         int rightIndex = 0;
         int curr = 0;
-        double result = 0;
+        double result;
 
         while (rightIndex - leftIndex != k) {
             curr += nums[rightIndex];
@@ -11,21 +12,20 @@ class Solution {
         }
         
         result = (double) curr / k;
+        
         if (nums.length == k) {
-            return (double) curr / k;
+            return result;
         }
 
         while (rightIndex < nums.length) {
-
-
+            
             curr -= nums[leftIndex];
-
-
             curr += nums[rightIndex];
+            
             rightIndex++;
             leftIndex++;
+            
             result = Math.max(result, (double) curr / k);
-
         }
         return result;
 
