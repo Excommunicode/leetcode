587c892e8d715f49d2f6781d4b7199763b85254c
Excommunicode <142714814+Excommunicode@users.noreply.github.com>
2024-11-04 20:16:32 +0500

    Time: 0 ms, Memory: 41.6 MB - LeetHub

--- jewels-and-stones/jewels-and-stones.java
+++ jewels-and-stones/jewels-and-stones.java
@@ -1,6 +1,5 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> countStones = new HashMap<>();
         int ans = 0;
         
         for (int i = 0; i < stones.length(); i++) {
